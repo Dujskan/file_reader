8e43c72d11ee2ca7da293859302067c288e23628
dujskan <sebbelundin@hotmail.com>
2018-12-28 10:06:43 +0100

    details..

--- app.mjs
+++ app.mjs
@@ -1,15 +1,15 @@
-import { fileReader } from './fileReader.mjs';
+import {
+    fileReader
+} from './fileReader.mjs';
 
 const main = async () => {
-    console.log(fileReader);
-    let input = await fileReader('./day3.txt', 'utf8')
+    await fileReader('./text.txt', 'utf8')
         .then((val) => {
             return val;
         })
         .catch((reason) => {
             throw console.log(reason);
         });
-    console.log(input);
 }
 
 main();
